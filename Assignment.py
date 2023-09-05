b93cb657146f536463173e93226019baa59f01c5
Cybber-404 <140195580+Cybber-404@users.noreply.github.com>
2023-09-05 22:02:03 +0000

    Create Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,9 @@
+for students in range(1, 17):
+    student_name = input(f'Enter the name of student NO.{students}: ')
+    score_1 = int(input('Enter test score 1: '))
+    score_2 = int(input('Enter test score 2: '))
+    score_3 = int(input('Enter test score 3: '))
+    score_4 = int(input('Enter test score 4: '))
+    average = (score_1 + score_2 + score_3 + score_4) / 4
+    print(average)
+    print()
