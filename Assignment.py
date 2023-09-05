e44743ae9096e0d6c937e7255770deff1244fd36
Cybber-404 <140195580+Cybber-404@users.noreply.github.com>
2023-09-05 22:08:31 +0000

    Final work

--- Assignment.py
+++ Assignment.py
@@ -1,9 +1,8 @@
 for students in range(1, 17):
-    student_name = input(f'Enter the name of student NO.{students}: ')
+    student_name = input(f'\nEnter the name of student NO.{students}: ')
     score_1 = int(input('Enter test score 1: '))
     score_2 = int(input('Enter test score 2: '))
     score_3 = int(input('Enter test score 3: '))
     score_4 = int(input('Enter test score 4: '))
     average = (score_1 + score_2 + score_3 + score_4) / 4
     print(f"The average score of {student_name} is {average}")
-    print()
